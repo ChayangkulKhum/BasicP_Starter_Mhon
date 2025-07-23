a3cffadb824d7fde4c0b97975494caae4646d47f
Chayangkul Khumthurn <145888325+ChayangkulKhum@users.noreply.github.com>
2025-07-23 08:45:21 +0700

    Update Basic3.py

--- Basic3.py
+++ Basic3.py
@@ -1,29 +1,26 @@
 distance = int(input("ระยะทางเท่าไร :"))
+Price = 0
 
 if distance<5:
     print("Free")
 elif distance >= 5 and distance <= 50:
     print(distance)
-    print(10)
-    print(10 * 0.07)
-    print((10 * 0.07)+10)
+    Price = 10
 elif distance >= 51 and distance <= 100:
     print(distance)
-    print(15)
-    print(15 * 0.07)
-    print((15 * 0.07)+15)
+    Price = 15
 elif distance >= 101 and distance <= 300:
     print(distance) 
-    print(25)
-    print(25 * 0.07)
-    print((25 * 0.07)+25)
+    Price = 25
 elif distance >= 301 and distance <= 500:
     print(distance)
-    print(35)
-    print(35 * 0.07)
-    print((35 * 0.07)+35)
+    Price = 35
 else:
     print(distance)
-    print(45)
-    print(45 * 0.07)
-    print((45 * 0.07)+50)
+    Price = 45
+
+Vat = Price * 0.07
+Total = Price + Vat
+print(Price)
+print(Vat)
+print(Total)
