90a6e77885ad6c5260d74aa5c487b90b7569167a
Chayangkul Khumthurn <145888325+ChayangkulKhum@users.noreply.github.com>
2025-07-23 14:09:35 +0700

    Update Basic6.py

--- Basic6.py
+++ Basic6.py
@@ -25,31 +25,45 @@ axe = 6
 i = 1
 fighting = 0
 
-while fighting == 1 or fighting == 2:
+m_health = 50
+sword = 4
+bow = 5
+axe = 6
+i = 1
+fighting = 0
+
+def ImFighting(m_health , round):
+    global i
+    while m_health != 0 and i != round+1:
+        print("จะใช้อะไรตีมี sword , bow , axe:")
+        weapon = input("")
+        if weapon == "sword":
+            m_health -= sword
+        elif weapon == "bow":
+            m_health -= bow
+        elif weapon == "axe":
+            m_health -= axe
+        print("Monster เลิอดเหลือ:" , m_health)
+        
+        
+
+while fighting == 0:
     fight = int(input("ต่อสู้เลือก 1 ถ้าไม่เลือก 2:"))
     if fight == 1:
         round = int(input("จะตีกี่รอบ:")) 
+        ImFighting(m_health, round)
+        fighting = 1
 
     elif fight == 2:
         print("คุณหนีได้สำเร็จ")
+        break
     
     else:
         print("อะไรของมึงกุให้เลือก 1 กับ 2")
 
-def ImFighting(m_health):
-    while m_health <= 0 and i == round+1:
-        print("จะใช้อะไรตีมี sword , bow , axe:")
-        if weapon == "sword":
-            m_health - sword
-        elif weapon == "bow":
-            m_health - bow
-        elif weapon == "axe":
-            m_health - axe
-        print("Monster เลิอดเหลือ:" , m_health)
-        
-        if m_health == 0:
-            print("คุณชนะ")
-        else:
-            print("Monster เลือด +20")
-            m_health + 20
-        i = i + 1
+if m_health == 0:
+    print("คุณชนะ")
+else:
+    print("Monster เลือด +20")
+    m_health + 20
+    ImFighting(m_health, round)
